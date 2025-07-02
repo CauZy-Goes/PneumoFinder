4f6472aaaf0a46b9c911a839b05e541ddb0987be
CauZy-Goes <cauafariasdev@gmail.com>
2025-07-02 18:06:16 -0300

    feat: update footer

--- pneumo-finder-web/src/components/Footer.tsx
+++ pneumo-finder-web/src/components/Footer.tsx
@@ -3,29 +3,30 @@ import Image from 'next/image';
 
 export function Footer(){
     return (
-    <footer id="rodape"
+    <footer 
     className="logo-footer
         bg-[#2d73ff]
         px-4
-        py-8
+        pt-5
+        pb-8
         flex 
         flex-col
         items-center
         text-center
         border-t-[5px] 
         border-[#000342]">
-        <div>
-            <Image
-              src="/IMGs_PneumoFinder/header-img-Photoroom.png"
-              alt="Logo Com Nome do PneumoFinder"
-              width={400} // você pode ajustar esse valor conforme a imagem original
-              height={200} // ajuste proporcional à largura
-              className="w-[30%] h-auto mb-4"
-            />
-        </div>
+
+      <div className="flex justify-center">
+          <Image
+            src="/IMGs_PneumoFinder/header-img-Photoroom.png"
+            alt="Logo Com Nome do PneumoFinder"
+            width={300} // você pode ajustar esse valor conforme a imagem original
+            height={100} // ajuste proporcional à largura
+          />
+      </div>
 
 
-      <div className="flex justify-center mb-4">
+      <div className="flex justify-center">
         <a href="https://github.com/CauZy-Goes" target="_blank" aria-label="GitHub"
               className="mx-[0.7em] text-[3em] text-white no-underline transition-colors duration-300 ease-in-out hover:text-[#000342]">
           <FontAwesomeIcon icon={['fab', 'github']} />
@@ -41,7 +42,7 @@ export function Footer(){
       </div>
 
 
-        <p className="mt-4 text-[0.9em] text-white">© 2025 Cauã Goes - Projeto Acadêmico IA</p>
+        <p className="mt-2 text-[0.9em] text-white">© 2025 Cauã Goes - Projeto Acadêmico IA</p>
     </footer>
     );
 }
